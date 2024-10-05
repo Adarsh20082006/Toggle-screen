1012a99aab9b82ebf017d6b9e4baa8da3530951d
Adarsh M M <adarshakashmm@gmail.com>
2024-10-05 09:30:07 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isBlack, setIsBlack] = useState(false);
+
+  const handleToggle = () => {
+    setIsBlack(!isBlack);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${isBlack ? 'black' : 'white'}`}>
+      <button onClick={handleToggle}>
+        {isBlack ? 'Switch to White' : 'Switch to Black'}
+      </button>
     </div>
   );
 }
